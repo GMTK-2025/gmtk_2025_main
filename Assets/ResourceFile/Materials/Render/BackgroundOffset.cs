ade7af2a66ab5bfe4d0ac453b1a93c5795c01e9e
Tonny Peng <46444046+StudyPeng@users.noreply.github.com>
2025-08-02 02:51:15 +0800

    [Update] Create Prefab

--- Assets/ResourceFile/Materials/Render/BackgroundOffset.cs
+++ Assets/ResourceFile/Materials/Render/BackgroundOffset.cs
@@ -1,8 +1,10 @@
+using Unity.Cinemachine;
 using UnityEngine;
 
 public class BackgroundOffset : MonoBehaviour
 {
     private static readonly int CharacterWs = Shader.PropertyToID("CharacterWS");
+
     // TODO: 用Camera的 bounding 来确定 offset
     private const float _yOffset = 20f;
     private const float _xOffset = 23f;
@@ -10,16 +12,19 @@ public class BackgroundOffset : MonoBehaviour
     [SerializeField] private MeshRenderer _meshRenderer;
     [SerializeField] private Transform _character;
     private Material _material;
-    
-    [Header("MODIFY")] 
-    [SerializeField] private float _xOffsetSpeed = 1f;
+
+    [Header("MODIFY")] [SerializeField] private float _xOffsetSpeed = 1f;
     [SerializeField] private float _yOffsetSpeed = 1f;
 
-    [Header("DO NOT MODIFY")] 
-    [SerializeField] private int[] _triangles;
+    [Header("DO NOT MODIFY")] [SerializeField]
+    private int[] _triangles;
+
     [SerializeField] private Vector2[] _uvs;
     [SerializeField] private Vector3[] _vertices;
     private Vector3 _lastFramePos = Vector3.zero;
+    private CinemachineBrain _brain;
+    private float _height;
+    private float _width;
 
     private void Start()
     {
@@ -31,10 +36,19 @@ public class BackgroundOffset : MonoBehaviour
         float xOffset = (_character.position.x - _lastFramePos.x) * _xOffsetSpeed;
         float yOffset = (_character.position.y - _lastFramePos.y) * _yOffsetSpeed;
 
+        Vector3 minMin = new Vector3(_character.transform.position.x - _width, _vertices[0].y, 1f);
+        Vector3 maxMin = new Vector3(_character.transform.position.x + _width, _vertices[1].y, 1f);
+        Vector3 maxMax = new Vector3(_character.transform.position.x + _width, _vertices[2].y, 1f);
+        Vector3 minMax = new Vector3(_character.transform.position.x - _width, _vertices[3].y, 1f);
+
+        _vertices = new[] { minMin, maxMin, maxMax, minMax };
+
         _uvs[0] = new Vector2(_uvs[0].x + xOffset, _uvs[0].y + yOffset);
         _uvs[1] = new Vector2(_uvs[1].x + xOffset, _uvs[1].y + yOffset);
         _uvs[2] = new Vector2(_uvs[2].x + xOffset, _uvs[2].y + yOffset);
         _uvs[3] = new Vector2(_uvs[3].x + xOffset, _uvs[3].y + yOffset);
+
+        _meshFilter.mesh.vertices = _vertices;
         _meshFilter.mesh.uv = _uvs;
         _meshRenderer.sharedMaterial.SetVector(CharacterWs, _character.position);
         _lastFramePos = _character.position;
@@ -42,11 +56,19 @@ public class BackgroundOffset : MonoBehaviour
 
     private void InitMaterial()
     {
+        _brain = Camera.main.GetComponent<CinemachineBrain>();
         Mesh mesh = new Mesh();
-        Vector3 minMin = new Vector3(_character.position.x - _xOffset, _character.position.y - _yOffset, 0f);
-        Vector3 maxMin = new Vector3(_character.position.x + _xOffset, _character.position.y - _yOffset, 0f);
-        Vector3 maxMax = new Vector3(_character.position.x + _xOffset, _character.position.y + _yOffset, 0f);
-        Vector3 minMax = new Vector3(_character.position.x - _xOffset, _character.position.y + _yOffset, 0f);
+        _height = _brain.OutputCamera.orthographicSize + 7f;
+        _width = _brain.OutputCamera.aspect * _brain.OutputCamera.orthographicSize;
+
+        Vector3 minMin = new Vector3(_character.transform.position.x - _width,
+            _character.transform.position.y - _height / 4, 1f);
+        Vector3 maxMin = new Vector3(_character.transform.position.x + _width,
+            _character.transform.position.y - _height / 4, 1f);
+        Vector3 maxMax = new Vector3(_character.transform.position.x + _width,
+            _character.transform.position.y + _height, 1f);
+        Vector3 minMax = new Vector3(_character.transform.position.x - _width,
+            _character.transform.position.y + _height, 1f);
 
         _triangles = new[] { 0, 1, 2, 0, 2, 3 };
         _vertices = new[] { minMin, maxMin, maxMax, minMax };
